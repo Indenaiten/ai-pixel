ee54dabe9bd13197191af4517af34a048bbcbf98
Ángel Herce Soto <indenaiten@gmail.com>
2024-09-24 20:11:16 +0200

    feat(common): Se ha creado una interfaz Entity
    - Se ha creado una interfaz "Entity" para definir las entidades de la aplicación.

---
+++ src/main/java/com/aipixel/api/common/entity/Entity.java
@@ -0,0 +1,12 @@
+package com.aipixel.api.common.entity;
+
+import java.io.Serializable;
+
+
+
+/**
+ * Interfaz que representa una entidad de la aplicación.
+ */
+public interface Entity extends Serializable {
+
+}
