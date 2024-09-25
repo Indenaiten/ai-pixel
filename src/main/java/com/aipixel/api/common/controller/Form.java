cd31ec9b99421ed1f8e478758ec8a719fb2cd2a6
Ángel Herce Soto <indenaiten@gmail.com>
2024-09-25 22:15:48 +0200

    feat(common): Se ha creado la interfaz Form
    - Se ha creado la interfaz "Form" para definir las clases que representen los datos que se reciben en los endpoints.

---
+++ src/main/java/com/aipixel/api/common/controller/Form.java
@@ -0,0 +1,11 @@
+package com.aipixel.api.common.controller;
+
+import java.io.Serializable;
+
+
+/**
+ * Interfaz que representa los datos recibidos en los endpoints de la API REST de la aplicación.
+ */
+public interface Form extends Serializable {
+
+}
