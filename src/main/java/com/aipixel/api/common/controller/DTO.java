e5d7d02aeb78f33bb06c77e68f44ec800f243107
Ángel Herce Soto <indenaiten@gmail.com>
2024-09-25 22:15:26 +0200

    feat(common): Se ha creado la interfaz DTO
    - Se ha creado la interfaz "DTO" para definir las clases que representen la información que se va a devolver en los endpoints de la aplicación.

---
+++ src/main/java/com/aipixel/api/common/controller/DTO.java
@@ -0,0 +1,12 @@
+package com.aipixel.api.common.controller;
+
+import java.io.Serializable;
+
+
+/**
+ * Interfaz que representa un DTO (Data Transfer Object) que se utiliza para devolver la información requerida en los
+ * endpoints de la API REST de la aplicación.
+ */
+public interface DTO extends Serializable {
+
+}
