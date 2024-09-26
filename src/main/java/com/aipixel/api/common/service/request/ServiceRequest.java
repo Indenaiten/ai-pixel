90b65fb7abd4974cf833a9906f7223f937bdcc7e
Ángel Herce Soto <indenaiten@gmail.com>
2024-09-26 09:02:02 +0200

    feat(common): Se ha creado el ServiceRequest
    - Se ha creado la interfaz "ServiceRequest" para definir los objetos que representan un conjunto de datos que se requieren en una petición a un servicio de la aplicación.

---
+++ src/main/java/com/aipixel/api/common/service/request/ServiceRequest.java
@@ -0,0 +1,11 @@
+package com.aipixel.api.common.service.request;
+
+import java.io.Serializable;
+
+
+/**
+ * Interfaz que representa un conjunto de datos necesarios para realizar una petición en los servicios de la aplicación.
+ */
+public interface ServiceRequest extends Serializable {
+
+}
